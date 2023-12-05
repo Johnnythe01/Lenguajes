14fd4a5ead7fa571ae408fe8585cf389c849a18a
John <jbarragandela@nigul.cide.es>
2023-12-05 19:10:08 +0100

    ex1 y 2 doneh

--- Actividades/Actividad_21/Ex2.js
+++ Actividades/Actividad_21/Ex2.js
@@ -1,7 +1,20 @@
-function celsiusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-  }
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+  rl.question('Indica cuantos grados de temperatura quieres convertir: ', (answer) => {
+    rl.question('Elige el formato de temperatura que quieras obtener: ', (tipo) => {
+    if (tipo == "Fahrenheit") {
+      answer = (answer * 9/5 + 32).toFixed(2);
+        console.log(`La temperatura en Fahrenheit es: ${answer}°F`);
+    } else {
+      answer = (answer -32 * 5/9).toFixed(2);
+        console.log(`La temperatura en Celsius es: ${answer}°C`);
+    }
   
-  function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-  }
+    rl.close();
+    })
+});
