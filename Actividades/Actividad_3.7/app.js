300c97e291431761aac65728199a7797ee431296
John <jbarragandela@nigul.cide.es>
2024-01-15 20:42:48 +0100

    3.7 avances

---
+++ Actividades/Actividad_3.7/app.js
@@ -0,0 +1,9 @@
+    let navigationbar = document.getElementById('navigationbar')
+    let boton = document.getElementById("hamburguesa")
+    
+    boton.addEventListener("click", function () {
+    
+        navigationbar.classList.toggle("navigationbar")
+        navigationbar.classList.toggle("navigationbar2")
+})
+
