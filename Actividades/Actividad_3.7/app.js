69c3ccd12c3bbcaee7f57bd269c0ccdd5e87b993
John <jbarragandela@nigul.cide.es>
2024-01-16 18:25:15 +0100

    3.7

--- Actividades/Actividad_3.7/app.js
+++ Actividades/Actividad_3.7/app.js
@@ -1,9 +1,11 @@
-    let navigationbar = document.getElementById('navigationbar')
-    let boton = document.getElementById("hamburguesa")
-    
-    boton.addEventListener("click", function () {
-    
-        navigationbar.classList.toggle("navigationbar")
-        navigationbar.classList.toggle("navigationbar2")
-})
+let navigationbar = document.getElementById('navigationbar')
+let boton = document.getElementById("hamburguesa")
+let option0 = document.getElementById("option0")
+let option1 = document.getElementById("option1")
+let option2 = document.getElementById("option2")
+let option3 = document.getElementById("option3")
+let option4 = document.getElementById("option4")
+boton.addEventListener("click", function () {
 
+    navigationbar.classList.toggle("visible")
+})
