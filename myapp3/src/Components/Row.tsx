35aa2fa10a65091ed92345c95ae2972b9ec1bae1
Johnny <you@example.com>
2024-02-21 22:28:48 +0100

    ss

--- myapp3/src/Components/Row.tsx
+++ myapp3/src/Components/Row.tsx
@@ -1,7 +1,36 @@
 import React from "react";
 
-function Row() {
-
-
-    return <></>;
+interface RowProps {
+    producto: string;
+    cantidad: number;
+    precio: number;
+    tienda: string;
+    notas: string;
+    comprado: boolean;
+    imagen: string;
 }
+const Row: React.FC<RowProps> = ({
+    producto,
+    cantidad,
+    precio,
+    tienda,
+    notas,
+    comprado,
+    imagen
+}) => {
+    return (
+        <tr>
+            <td>{producto}</td>
+            <td>{cantidad}</td>
+            <td>{precio}</td>
+            <td>{tienda}</td>
+            <td>{notas}</td>
+            <td>{comprado ? "Sí" : "No"}</td>
+            <td>
+                <img src={imagen} alt={producto} style={{ width: "50px", height: "50px" }} />
+            </td>
+        </tr>
+    );
+}
+
+export default Row;
