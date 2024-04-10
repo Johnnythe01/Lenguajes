396b92aabc47785da5ef6b530f54af7a2f55fa13
John <jbarragandela@nigul.cide.es>
2024-04-10 18:53:12 +0200

    ss

--- myapp6/src/pages/Fantasia.tsx
+++ myapp6/src/pages/Fantasia.tsx
@@ -17,7 +17,7 @@ export interface Arma {
 }
 
 
-function Fantasia () {
+function Fantasia() {
   const [armas, setArmas] = React.useState([] as Armas)
 
   React.useEffect(() => {
@@ -33,23 +33,28 @@ function Fantasia () {
     <Container>
       <Row xs={1} sm={2} md={3} lg={4} className='g-4'>
         {armas.map((arma, i) => (
+
           <Col key={i}>
-            <Link to={`/Fantasia/${arma.id}`}>
-              <Card className="card-body2">
-                <Card.Body>
-                  <Card.Title>{arma.nombre}</Card.Title>
+            <div className="row text-center">
+              <Link to={`/Fantasia/${arma.id}`}>
+                <Card className="card-body2">
+                  <Card.Body>
+                    <Card.Title>{arma.nombre}</Card.Title>
+                  </Card.Body>
+                  {(
+                    <Image
+                      src={arma.imageUrl}
+                      alt={arma.nombre}
+                      fluid
+                      style={{ maxWidth: '200px', height: '260px', margin: '0 auto' }}
+                    />
+                  )}
                   <Card.Text>{arma.origen}</Card.Text>
-                </Card.Body>
-                {(
-                  <Image
-                    src={arma.imageUrl}
-                    alt={arma.nombre}
-                    fluid
-                    style={{ maxWidth: '200px', height: 'auto' }}
-                  />
-                )}
-              </Card>
-            </Link>
+                  <Card.Text>{arma.disponible ? "Disponible" : "No disponible"} </Card.Text>
+                  <Card.Text>{arma.id}</Card.Text>
+                </Card>
+              </Link>
+            </div>
           </Col>
         ))}
       </Row>
--- myapp6/src/pages/home.tsx
+++ myapp6/src/pages/home.tsx
@@ -9,7 +9,7 @@ function Inicio() {
                 <p>Aquí tendrás la oportunidad de saber que estilo se adapta más a ti, y encontrar maneras de conseguir
                     esos artefactos legendarios que te convertirán en el nº1.</p>
                 <p>No nos hacemos responsables de los daños que pueda ocasionar enfrentar enemigos con más de 100 millones de vida.</p>
-                <img src="https://i.pinimg.com/originals/af/fc/ef/affcefdc5c8338d91918c5db0f168fda.jpg" alt="" />
+                <img src="https://i.pinimg.com/originals/af/fc/ef/affcefdc5c8338d91918c5db0f168fda.jpg" alt="1300px" width="1300px"/>
             </div>
 
     );
