c0996aeace6094d7d0f6889157b846918a39b0ad
John <jbarragandela@nigul.cide.es>
2024-05-15 18:09:36 +0200

    ss

---
+++ clase-express6.3/index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const db = require('better-sqlite3')('personas.sqlite')
+
+const app = express()
+const port = 3000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index", msgs = { msgs: ["Hola", "desde", "la", "ruta"] });
+})
+
+app.get('/vistausuarios', (req, res) => {
+    const rows = db.prepare('SELECT * from usuarios').all();
+    res.render("index", msgs = { msgs: rows })
+})
+
+app.get('/patata', (req, res) => {
+    res.send('Hello World!')
+})
+
+// devolveremos el render de la vista con un form
+app.get('/personas', (req, res) => {
+    const rows = db.prepare('SELECT * from personas').all();
+    res.render('personas',  personas = rows )
+})
+
+app.get('/usuario', (req, res) =>{
+    res.render("usuario");
+})
+
+//capturaremos el submit del form
+app.post("/usuario", (req, res) => {
+    console.log(req.body);
+    if (req.body.nombre && req.body.email) {
+        //insert
+        const statement = db.prepare("INSERT INTO usuarios (nombre, email) VALUES (?, ?)")
+        const info = statement.run(req.body.nombre, req.body.email);
+        console.log(info);
+    }
+    res.redirect("usuario");
+})
+
+app.get('/productos', (req, res) => {
+    const rows = db.prepare('SELECT * from productos').all();
+    res.render('productos',  productos = rows )
+})
+
+//capturaremos el submit del form
+app.post("/producto", (req, res) => {
+    console.log(req.body);
+    if (req.body.nombre && req.body.precio) {
+        //insert
+        const statement = db.prepare("INSERT INTO productos (nombre, precio) VALUES (?, ?)")
+        const info = statement.run(req.body.nombre, req.body.precio);
+        console.log(info);
+    }
+    res.redirect("producto");
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/usuarios', (req, res) => {
+    const rows = db.prepare('SELECT * from usuarios').all();
+    res.json(rows)
+})
+
+app.get('/productos', (req, res) => {
+    const rows = db.prepare('SELECT * from productos').all();
+    res.json(rows)
+})
+
+app.get('/comandas', (req, res) => {
+    const rows = db.prepare('SELECT * from comandas').all();
+    res.json(rows)
+})
